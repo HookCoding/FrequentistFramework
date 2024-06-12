c79441992750241ee88595afbfb79a54e47d74dd
agekow <alex.gekow@cern.ch>
2024-06-12 11:11:30 +0200

    Update submodules to track tla-run-3 forks

--- scripts/install_pyBumpHunter.sh
+++ scripts/install_pyBumpHunter.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 cd pyBumpHunter
-virtualenv --python=/cvmfs/sft.cern.ch/lcg/releases/LCG_96bpython3/Python/3.6.5/x86_64-centos7-gcc8-opt/bin/python3 pyBH_env  # create a venv w/ python3 (if we don’t use --python=... then it defaults to python 2.7)
+virtualenv --python=/cvmfs/sft.cern.ch/lcg/releases/LCG_105/Python/3.9.12/x86_64-el9-gcc11-opt/lib/python3.9/site.py pyBH_env  # create a venv w/ python3 (if we don’t use --python=... then it defaults to python 2.7)
 source pyBH_env/bin/activate # activate it 
 # installing everything
 pip install --upgrade pip
